7df618d764625a652f27b64af8d45ad24d511198
KallDrexx <me@mshapiro.net>
2016-07-30 16:28:14 -0400

    Added implementation that supports non-object amf0 values

--- amf0/src/serialization/mod.rs
+++ amf0/src/serialization/mod.rs
@@ -1,15 +1,51 @@
-use super::Amf0Value;
+//! Module contains functionality for serializing values into an
+//! bytes based on the AMF0 specification 
+//! (http://wwwimages.adobe.com/content/dam/Adobe/en/devnet/amf/pdf/amf0-file-format-specification.pdf)
 
-/// Serializes values into an amf0 encoded vector of bytes
-pub fn serialize(values: &Vec<Amf0Value>) -> Vec<u8> {
-    vec![]
-}
+use super::Amf0Value;
+use byteorder::{BigEndian, WriteBytesExt};
 
 const NUMBER_MARKER: u8 = 0;
 const BOOLEAN_MARKER: u8 = 1;
 const STRING_MARKER: u8 = 2;
 const OBJECT_MARKER: u8 = 3;
 const NULL_MARKER: u8 = 5; 
+
+/// Serializes values into an amf0 encoded vector of bytes
+pub fn serialize(values: &Vec<Amf0Value>) -> Vec<u8> {
+    let mut result = vec![];
+    for value in values {
+        match *value {
+            Amf0Value::Boolean(ref val) => serialize_bool(&val, &mut result),
+            Amf0Value::Null => serialize_null(&mut result),
+            Amf0Value::Number(ref val) => serialize_number(&val, &mut result),
+            Amf0Value::Utf8String(ref val) => serialize_string(&val, &mut result),
+            Amf0Value::Object(_) => panic!("Not Implemented")
+        }
+    }
+
+    result
+}
+
+fn serialize_number(value: &f64, vector: &mut Vec<u8>) {
+    vector.push(NUMBER_MARKER);
+    vector.write_f64::<BigEndian>(value.clone()).unwrap();    
+}
+
+fn serialize_bool(value: &bool, vector: &mut Vec<u8>) {
+    vector.push(BOOLEAN_MARKER);
+    vector.push((value.clone()) as u8);
+}
+
+fn serialize_string(value: &String, vector: &mut Vec<u8>) {
+    vector.push(STRING_MARKER);
+    vector.write_u16::<BigEndian>(value.len() as u16).unwrap(); // TODO: add check if length is > u16
+    vector.extend(value.as_bytes());
+}
+
+fn serialize_null(vector: &mut Vec<u8>) {
+    vector.push(NULL_MARKER);
+}
 
 #[cfg(test)]
 mod tests {
